5ab44e0dfcacf5f40f5c5dc9b3c9ac95726f4799
Aurino Jr <allexandrejr@gmail.com>
2014-07-21 14:31:19 -0300

    adding in templates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
 
 # initialization
@@ -10,9 +10,17 @@ app.config.update(
 
 
 # controllers
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(os.path(app.root_path, 'static'), 'ico/favicon.ico')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return render_template('index.html')
 
 
 # launch
